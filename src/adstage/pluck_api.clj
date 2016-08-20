10536c86b6791954ae6afa2381101a375a0d71ef
Karan Toor <karanbirsinghtoor@gmail.com>
2016-08-20 15:15:13 -0700

    Added source and some tests

---
+++ src/adstage/pluck_api.clj
@@ -0,0 +1,43 @@
+(ns adstage.pluck-api
+  (:require
+   [adstage.pluck-helper :refer [-pluck]]
+   [datomic.api :as d]))
+
+(defn pick
+  "Recursively annotates the init-result with values supplied from the `-pluck`
+  multimethod."
+  [env pattern init-result]
+  (->> pattern
+       (map (fn [k]
+              [(cond
+                 (keyword? k) k
+                 (map? k)     (-> k keys first))
+               (cond
+                 (keyword? k) (-pluck k env init-result)
+
+                 (and (map? k)
+                      (vector? (get init-result (-> k keys first))))
+                 (mapv #(pick env (-> k vals first) %) (get init-result (-> k keys first)))
+
+                 (and (map? k)
+                      (get init-result (-> k keys first)))
+                 (pick env (-> k vals first) (get init-result (-> k keys first)))
+
+                 (and (map? k)
+                      (nil? (get init-result (-> k keys first))))
+                 [])]))
+       (into {})))
+
+(defn pluck
+  "A more generic version of the datomic.api/pull. Takes an environment
+  as the first argument, you can either pass in a datomic value or database
+  component (priority given to former).
+  Second argument is a pull style query. Third argument is an entity id or
+  entity map (from a previous pull/pluck call).
+  Specific key lookup is implemented via -pluck multimethod. Default case is
+  datomic.api/pull."
+  [{:keys [db] :as env} pattern eid-or-map]
+  (let [init-result (if (map? eid-or-map)
+                      eid-or-map
+                      (d/pull db pattern eid-or-map))]
+    (pick env pattern init-result)))
---
+++ test/adstage/pluck_api_test.clj
@@ -0,0 +1,53 @@
+(ns adstage.pluck-api-test
+  (:require [adstage.pluck-api :as p]
+            [adstage.pluck-helper :refer [defmethod-cached] :as h]
+            [clojure.test :refer :all]))
+
+(defmethod h/-pluck :data-source/blob [k {store :blob-store} {eid :db/id}]
+  (store eid))
+
+(defmethod-cached h/-pluck :dashboard/refreshed-at [k env init-result]
+  (throw
+   (Exception. "Should never get here if :dashboard/refreshed-at is passed in.")))
+
+(deftest pluck-api-test
+  (testing "Basic nested Datomic pull result traversal, no pluck extension."
+    (let [query       [:dashboard/title
+                       {:dashboard/widgets
+                        [:db/id :widget/title
+                         {:widget/data-source [:data-source/owner]}]}
+                       {:dashboard/author [:user/first-name]}]
+          init-result {:dashboard/title  "Such Dashing Wow!!!"
+                       :dashboard/widgets
+                       [{:db/id              17592186057566
+                         :widget/title       "Unnamed"
+                         :widget/data-source {:data-source/owner {:db/id 17592186045435}}}
+                        {:db/id              17592186057638
+                         :widget/title       "Metered Metrics"
+                         :widget/data-source {:data-source/owner {:db/id 17592186045435}}}]
+                       :dashboard/author {:user/first-name "Clark"}}]
+      (is (= init-result
+             (p/pick {} query init-result)))))
+
+  (testing "Extending with a foreign blob store."
+    (let [query       [{:dashboard/widgets [{:widget/data-source [:db/id :data-source/blob]}]}]
+          init-result {:dashboard/widgets
+                       [{:widget/data-source {:db/id :db/id-1}}
+                        {:widget/data-source {:db/id :db/id-2}}]}
+
+          blob-store {:db/id-1 "first-blob"
+                      :db/id-2 "second-blob"}]
+      (is (= {:dashboard/widgets
+              [{:widget/data-source
+                {:db/id :db/id-1 :data-source/blob "first-blob"}}
+               {:widget/data-source
+                {:db/id :db/id-2 :data-source/blob "second-blob"}}]}
+             (p/pick {:blob-store blob-store} query init-result)))))
+
+  (testing "Cached extension should only be called if key missing from init-result."
+    (let [query       [:dashboard/refreshed-at]
+          init-result {:dashboard/refreshed-at #inst "2016-08-20T22:10:26.652-00:00"}]
+      (is (= {:dashboard/refreshed-at #inst "2016-08-20T22:10:26.652-00:00"}
+             (p/pick {} query init-result)))
+      (is (thrown-with-msg? Exception #"dashboard\/refreshed-at"
+                            (p/pick {} query {}))))))
